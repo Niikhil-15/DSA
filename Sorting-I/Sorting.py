1dd7b5e648ca5ff8cc40bb47442f0512d25ad78a
Nikhil Raj <rajnikhil4795@gmail.com>
2026-01-03 22:44:09 +0530

    bubble sort done

--- Sorting-I/Sorting.py
+++ Sorting-I/Sorting.py
@@ -1,3 +1,6 @@
+arr=[23,12,89,22,9,1]
+
+# Time Complexity - O(N²) for all cases
 def selection_sort(arr):
     for i in range(len(arr)-1):
         min_i=i
@@ -8,5 +11,34 @@ def selection_sort(arr):
     
     return arr
 
-arr=[23,12,89,22,9,1]
-print(selection_sort(arr))
+
+# print(selection_sort(arr))
+
+
+# Time Complexity - Worst Case and Average Case - O(N²) but best case is O(N) if given arr is already sorted 
+
+def bubble_sort(arr):
+    for i in range(len(arr)-1):
+        didSwap=0
+        for j in range(len(arr)-i-1):
+            if arr[j+1] < arr[j]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+                didSwap=1
+        if didSwap==0: break
+
+
+# print(arr)
+# bubble_sort(arr)
+# print(arr)
+
+# Insertion Sort
+
+def insertion_sort(arr):
+    for i in range(1,len(arr)+1):
+        for j in range(i-1):
+            if arr[j]> arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+            
+
+insertion_sort(arr)
+print(arr)
